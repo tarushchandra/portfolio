7f45fffb28ebf6d881462f1e0d553f238fc6dff7
Tarush Chandra <tarushchandra6@gmail.com>
2026-09-18 01:28:09 +0530

    fix: removed about page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 import { PageFrame } from "@/components/page-frame";
 import { Hero } from "@/components/home/hero";
 import { Work } from "@/components/home/work";
-import { About } from "@/components/home/about";
 import { Skills } from "@/components/home/skills";
 import { Contact } from "@/components/home/contact";
 
